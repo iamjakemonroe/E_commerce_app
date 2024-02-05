749a64a8f405a5016bbd36a1611df4e273361ba9
iamjakemonroe <iamjakemonroe@gmail.com>
2024-02-05 23:05:17 +0100

    new category file

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/constant.dart';
+import 'categories.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -19,78 +20,4 @@ class Body extends StatelessWidget {
       ],
     );
   }
-}
-
-class Categories extends StatefulWidget {
-  const Categories({super.key});
-
-  @override
-  State<Categories> createState() => _CategoriesState();
-}
-
-class _CategoriesState extends State<Categories>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  List<String> categories = ["Hand bag", "Jewerries", "Footwear", "Dresses"];
-  int selectedIndex = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-        height: 25,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: categories.length,
-          itemBuilder: (context, index) => buildCategory(
-              index, categories, selectedIndex, _updateSelectedIndex),
-        ));
-  }
-
-  void _updateSelectedIndex(int newIndex) {
-    setState(() {
-      selectedIndex = newIndex;
-    });
-  }
-}
-
-Widget buildCategory(int index, List<String> categories, int selectedIndex,
-    Function(int) updateSelectedIndex) {
-  return GestureDetector(
-    onTap: () {
-      updateSelectedIndex(index);
-    },
-    child: Padding(
-      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            categories[index],
-            style: TextStyle(
-              color: selectedIndex == index ? kTextColor : kTextLightColor,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Container(
-            height: 2,
-            width: 30,
-            color: selectedIndex == index ? Colors.black : Colors.transparent,
-            margin: const EdgeInsets.only(top: 3),
-          )
-        ],
-      ),
-    ),
-  );
 }
