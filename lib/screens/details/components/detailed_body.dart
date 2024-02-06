f1fccc81d7890aa61720d58e4a53e71421c059d8
iamjakemonroe <iamjakemonroe@gmail.com>
2024-02-06 14:26:12 +0100

    origin change

--- lib/screens/details/components/detailed_body.dart
+++ lib/screens/details/components/detailed_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_app/constant.dart';
 import 'package:shopping_app/models/Product.dart';
+import 'package:shopping_app/screens/details/components/title_image_detail.dart';
 
 class DetailedBody extends StatelessWidget {
   final Product product;
@@ -26,59 +26,7 @@ class DetailedBody extends StatelessWidget {
                     ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: kDefaultPadding,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'Aristocratic hand bag',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      Text(
-                        product.title,
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineSmall
-                            ?.copyWith(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
-                      ),
-                      // const SizedBox(
-                      //   height: kDefaultPadding,
-                      // ),
-                      Row(
-                        children: [
-                          RichText(
-                            text: TextSpan(
-                              children: [
-                                const TextSpan(text: "Price\n"),
-                                TextSpan(
-                                    text: '\$${product.price}',
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headlineSmall
-                                        ?.copyWith(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold))
-                              ],
-                            ),
-                          ),
-                          const SizedBox(
-                            width: kDefaultPadding,
-                          ),
-                          Expanded(
-                              child: Image.asset(
-                            product.image,
-                            fit: BoxFit.fill,
-                          ))
-                        ],
-                      )
-                    ],
-                  ),
-                )
+                TitleImageDetail(product: product)
               ],
             ))
       ]),
---
+++ lib/screens/details/components/title_image_detail.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/constant.dart';
+import 'package:shopping_app/models/Product.dart';
+
+class TitleImageDetail extends StatelessWidget {
+  const TitleImageDetail({
+    super.key,
+    required this.product,
+  });
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: kDefaultPadding,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Aristocratic hand bag',
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            product.title,
+            style: Theme.of(context)
+                .textTheme
+                .headlineSmall
+                ?.copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: kDefaultPadding,
+          ),
+          Row(
+            children: [
+              RichText(
+                text: TextSpan(
+                  children: [
+                    const TextSpan(text: "Price\n"),
+                    TextSpan(
+                        text: '\$${product.price}',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineSmall
+                            ?.copyWith(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold))
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: kDefaultPadding,
+              ),
+              Expanded(
+                  child: Image.asset(
+                product.image,
+                fit: BoxFit.fill,
+              ))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
